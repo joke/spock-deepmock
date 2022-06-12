2b7b3fc94c05a463c93a35393b38f30ce7a20fa1
Joke de Buhr <joke@xckk.de>
2022-06-12 12:16:14 +0200

    refactor: rebuild test setup
    Signed-off-by: Joke de Buhr <joke@xckk.de>

---
+++ examples/src/main/java/io/github/joke/test/Caller.java
@@ -0,0 +1,13 @@
+package io.github.joke.test;
+
+public class Caller {
+    private final Nested nested;
+
+    public Caller(final Nested nested) {
+        this.nested = nested;
+    }
+
+    public String call() {
+        return nested.getChild().getChild().getName();
+    }
+}
---
+++ examples/src/test/groovy/io/github/joke/test/GroovyMockTest.groovy
@@ -0,0 +1,72 @@
+package io.github.joke.test
+
+import org.spockframework.mock.MockUtil
+import spock.lang.Specification
+
+class GroovyMockTest extends Specification {
+
+    def mockUtil = new MockUtil()
+
+    GroovyNested first = GroovyDeepMock()
+
+    def 'check deep mock is returned'() {
+        setup:
+        first.name >> 'first'
+        first.child.name >> 'second'
+        first.child.child.name >> 'third'
+
+        expect:
+        mockUtil.isMock first
+        mockUtil.isMock first.child
+        mockUtil.isMock first.child.child
+
+        first.name == 'first'
+        first.child.name == 'second'
+        first.child.child.name == 'third'
+    }
+
+    def 'calling nested mock'() {
+        setup:
+        def caller = new GroovyCaller(first)
+
+        when:
+        def res = caller()
+
+        then:
+        1 * first.child.child.name >> 'Hello'
+
+        expect:
+        res == 'Hello'
+    }
+
+    def 'saving mocks in variables'() {
+        setup:
+        def caller = new GroovyCaller(first)
+        def second = first.child
+
+        when:
+        def res = caller.call()
+
+        then:
+        1 * second.child.name >> 'Hello'
+
+        expect:
+        res == 'Hello'
+        first.child == second
+    }
+
+    def 'naming mock'() {
+        setup:
+        GroovyNested namedMock = GroovyDeepMock(name: 'customName')
+        def caller = new GroovyCaller(namedMock)
+
+        when:
+        def res = caller.call()
+
+        then:
+        1 * namedMock.child.child.name >> 'Hello'
+
+        expect:
+        res == 'Hello'
+    }
+}
